485007c8edd2a2a1980bd4e9e320a76a2f0bb641
quiwaan <quiwaan@me.com>
2018-12-18 17:32:27 -0800

    Learning Node

---
+++ wdi-22-code/unit2/node-views-proj/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var app = express()
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname+'/views/index.html');
+});
+
+
+app.get("/about", function(req, res){
+	res.sendFile(__dirname+'/views/about.html');
+});
+
+
+app.get("/blog", function(req, res){
+	res.sendFile(__dirname+'/views/blog-directory.html');
+});
+
+
+app.listen(8000);
